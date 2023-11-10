1dc11899307637ff1b4b132efee270616048bf26
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-10 10:34:42 +0530

    Create a function to display console on screen

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
-// let buttons = document.querySelectorAll('.numberButton, .operationButton, .equalButton, .deleteButton, .decimalButton, .clearButton');
-// let clickedValue;
-// let firstOperand = '';
-// let secondOperand = '';
-// let operators = '';
-// let equalsOperator = '';
-
-//   buttons.forEach(function(button) {
-//     button.addEventListener('click', handleClick);
-//   });
-
-//   function handleClick(event)
-//    {
-// clickedValue = event.target.textContent;
-
-// // Should Return true if it's a Number.
-// const isNumber = !isNaN(parseFloat(clickedValue));
-
-// const isOperator = clickedValue !== ('=' || 'Clear' || 'Delete')
-
-// const firstOperandCondition = isNumber && operators.length === 0
-
-// const operatorsCondition = (!isNumber && operators.length === 0 ) && isOperator;
-
-// const secondOperandCondition = ((isNumber && operators.length === 1) && equalsOperator === '') ;
-
-// let display = firstOperandCondition ?  firstOperand += clickedValue :
-//               operatorsCondition ? operators += clickedValue :
-//               secondOperand += clickedValue;
-
-//               let updatedDisplay = (operators === '') ? console.log((display += clickedValue).slice(0,-1)) :
-//                      (secondOperand === '') ? console.log(`${parseFloat(firstOperand)}${operators}`) :
-//                      console.log(`${parseFloat(firstOperand)}${operators}${parseFloat(secondOperand)}` );
-
-
-// return
-//   }
-
 let buttons = document.querySelectorAll('.numberButton, .operationButton, .equalButton, .deleteButton, .decimalButton, .clearButton');
+const displayScreen = document.querySelector('.display');
+
+function getDisplay(value) {
+  // Set the inner text of the display element
+  displayScreen.innerText = value;
+}
+
 let clickedValue;
 let firstOperand = '';
 let secondOperand = '';
@@ -68,7 +37,9 @@ function handleClick(event) {
                       (secondOperand === '') ? `${parseFloat(firstOperand)}${operators}` :
                       `${parseFloat(firstOperand)}${operators}${parseFloat(secondOperand)}`;
 
-  console.log(updatedDisplay);
+  // console.log(updatedDisplay);
+
+  getDisplay(updatedDisplay)
 
   return;
 }
