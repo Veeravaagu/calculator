87857e460c952c4c8e7901820a52a1724126a835
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-10 15:25:06 +0530

    Create a functionality for deleteButton.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const buttons = document.querySelectorAll('.numberButton, .operationButton, .equ
 const displayScreen = document.querySelector('.display');
 
 function getDisplay(value) {
-  // Set the inner text of the display element
   displayScreen.innerText = value;
 }
 
@@ -13,108 +12,121 @@ let operators = '';
 let equalsOperator = '';
 let result;
 
-buttons.forEach(function(button) {
+buttons.forEach(function (button) {
   button.addEventListener('click', handleClick);
 });
 
 function handleClick(event) {
   clickedValue = event.target.textContent;
 
-  // Should return true if it's a number.
   const isNumber = !isNaN(parseFloat(clickedValue));
-
-  // Should return true if it's not '=', 'Clear', or 'Delete'.
   const isOperator = clickedValue !== '=' && clickedValue !== 'Clear' && clickedValue !== 'Delete';
 
-  const firstOperandCondition = isNumber && operators.length === 0;
-  const operatorsCondition = !isNumber && operators.length === 0 && isOperator;
+  const firstOperandCondition = isNumber && operators.length === 0 && equalsOperator === '';
+  const operatorsCondition = !isNumber && operators.length === 0 && isOperator && equalsOperator === '';
   const secondOperandCondition = isNumber && operators.length === 1 && equalsOperator === '';
 
-  let display = firstOperandCondition ?  firstOperand += clickedValue :
-                operatorsCondition ? operators += clickedValue :
-                secondOperand += clickedValue;
+  let display = firstOperandCondition ? firstOperand += clickedValue :
+    operatorsCondition ? operators += clickedValue :
+      secondOperand += clickedValue;
 
-  let updatedDisplay = (operators === '') ? (display += clickedValue).slice(0,-1) :
-                      (secondOperand === '') ? `${parseFloat(firstOperand)}${operators}` :
-                      `${parseFloat(firstOperand)}${operators}${parseFloat(secondOperand)}`;
+  let updatedDisplay = (operators === '') ? (display += clickedValue).slice(0, -1) :
+    (secondOperand === '') ? `${parseFloat(firstOperand)}${operators}` :
+      `${parseFloat(firstOperand)}${operators}${parseFloat(secondOperand)}`;
 
-  getDisplay(updatedDisplay)
+  getDisplay(updatedDisplay);
 
-    // Perform the calculation when '=' is clicked
-    if (clickedValue === '=' && operators !== '' && firstOperand !== '' && secondOperand !== '') {
-      calculate();
-    }
-  
-    return;
+  if (clickedValue === '=' && operators !== '' && firstOperand !== '' && secondOperand !== '') {
+    calculate();
+    equalsOperator = '=';
+  } else if (clickedValue === 'Delete') {
+    handleDelete();
+  } else if (clickedValue === 'Clear') {
+    isClear();
   }
-  
-  
-  function calculate() {
-    // Convert operands to numbers
-    const a = parseFloat(firstOperand);
-    const b = parseFloat(secondOperand);
-  
-    // Perform the calculation based on the operator
-    switch (operators) {
-      case '+':
-        result = isAdd(a, b);
-        break;
-      case '-':
-        result = isSubtract(a, b);
-        break;
-      case 'x':
-        result = isMultiply(a, b);
-        break;
-      case '/':
-        result = isDivide(a, b);
-        break;
-        case 'Clear':
-        result = isClear();
-        break;
-        case 'Delete':
-        result = isDelete(a, b);
-        break;
-      default:
 
-        break;
-    }
-  
-    // Update the display with the result
-    getDisplay(result);
-  
-    // Reset operands and operator
-    firstOperand = result.toString();
-    secondOperand = '';
+  return;
+}
+
+function calculate() {
+  const a = parseFloat(firstOperand);
+  const b = parseFloat(secondOperand);
+
+  switch (operators) {
+    case '+':
+      result = isAdd(a, b);
+      break;
+    case '-':
+      result = isSubtract(a, b);
+      break;
+    case 'x':
+      result = isMultiply(a, b);
+      break;
+    case '/':
+      result = isDivide(a, b);
+      break;
+    case 'Clear':
+      result = isClear();
+      break;
+    default:
+      break;
+  }
+
+  getDisplay(result);
+
+  firstOperand = result.toString();
+  secondOperand = '';
+  operators = '';
+}
+
+function handleDelete() {
+  if (firstOperand !== '' && secondOperand !== '' && operators !== '' && equalsOperator === '') {
+    secondOperand = secondOperand.slice(0, -1);
+  } else if (operators !== '' && equalsOperator === '') {
     operators = '';
+  } else if (firstOperand !== '' && equalsOperator === '') {
+    // Check if deleting the last character would result in an empty string
+    firstOperand = (firstOperand.length > 1) ? firstOperand.slice(0, -1) : '0';
+  } else if (firstOperand === '' && secondOperand === '' && operators === '') {
+    // Display 0 when attempting to delete from an empty input
+    getDisplay('0');
   }
-  
-  function isAdd(a, b) {
-    return a + b;
-  }
-  
-  function isSubtract(a, b) {
-    return a - b;
-  }
-  
-  function isMultiply(a, b) {
-    return a * b;
-  }
-  
-  function isDivide(a, b) {
-    // Handle division by zero
-    return b !== 0 ? a / b : 'Error';
-  }
-  function isClear() {
-    firstOperand = '';
-    secondOperand = '';
-    operators = '';
-    equalsOperator = '';
-    return
-  }
-  function isDelete(a, b) {
-    return ;
-  }
-  
+
+  getDisplay(getUpdatedExpression());
+}
+
+
+function isClear() {
+  firstOperand = '';
+  secondOperand = '';
+  operators = '';
+  equalsOperator = '';
+  getDisplay('0');
+}
+
+function isAdd(a, b) {
+  return a + b;
+}
+
+function isSubtract(a, b) {
+  return a - b;
+}
+
+function isMultiply(a, b) {
+  return a * b;
+}
+
+function isDivide(a, b) {
+  return b !== 0 ? a / b : 'Error';
+}
+
+function getUpdatedExpression() {
+  return secondOperand !== '' ? `${parseFloat(firstOperand)}${operators}${parseFloat(secondOperand)}` :
+    operators !== '' ? `${parseFloat(firstOperand)}${operators}` :
+      firstOperand;
+}
+
+
 
 
 
