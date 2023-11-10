700d28385429c0e14326943e04463dd0dd7f0d98
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-10 10:28:43 +0530

    Clean the code and Create updatedDisplay and Comments

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+// let buttons = document.querySelectorAll('.numberButton, .operationButton, .equalButton, .deleteButton, .decimalButton, .clearButton');
+// let clickedValue;
+// let firstOperand = '';
+// let secondOperand = '';
+// let operators = '';
+// let equalsOperator = '';
+
+//   buttons.forEach(function(button) {
+//     button.addEventListener('click', handleClick);
+//   });
+
+//   function handleClick(event)
+//    {
+// clickedValue = event.target.textContent;
+
+// // Should Return true if it's a Number.
+// const isNumber = !isNaN(parseFloat(clickedValue));
+
+// const isOperator = clickedValue !== ('=' || 'Clear' || 'Delete')
+
+// const firstOperandCondition = isNumber && operators.length === 0
+
+// const operatorsCondition = (!isNumber && operators.length === 0 ) && isOperator;
+
+// const secondOperandCondition = ((isNumber && operators.length === 1) && equalsOperator === '') ;
+
+// let display = firstOperandCondition ?  firstOperand += clickedValue :
+//               operatorsCondition ? operators += clickedValue :
+//               secondOperand += clickedValue;
+
+//               let updatedDisplay = (operators === '') ? console.log((display += clickedValue).slice(0,-1)) :
+//                      (secondOperand === '') ? console.log(`${parseFloat(firstOperand)}${operators}`) :
+//                      console.log(`${parseFloat(firstOperand)}${operators}${parseFloat(secondOperand)}` );
+
+
+// return
+//   }
+
 let buttons = document.querySelectorAll('.numberButton, .operationButton, .equalButton, .deleteButton, .decimalButton, .clearButton');
 let clickedValue;
 let firstOperand = '';
@@ -5,54 +43,35 @@ let secondOperand = '';
 let operators = '';
 let equalsOperator = '';
 
-  buttons.forEach(function(button) {
-    button.addEventListener('click', handleClick);
-  });
+buttons.forEach(function(button) {
+  button.addEventListener('click', handleClick);
+});
 
-  function handleClick(event)
-   {
-clickedValue = event.target.textContent;
+function handleClick(event) {
+  clickedValue = event.target.textContent;
 
-const firstOperandCondition = (isNaN(parseFloat(clickedValue)) === false && operators.length === 0);
+  // Should return true if it's a number.
+  const isNumber = !isNaN(parseFloat(clickedValue));
 
-const operatorsCondition = ((isNaN(parseFloat(clickedValue)) === true && operators.length < 1) && clickedValue !== ('=' || 'Clear' || 'Delete'));
+  // Should return true if it's not '=', 'Clear', or 'Delete'.
+  const isOperator = clickedValue !== '=' && clickedValue !== 'Clear' && clickedValue !== 'Delete';
 
-const secondOperandCondition = ((isNaN(parseFloat(clickedValue)) === false && operators.length === 1) && equalsOperator === '') ;
+  const firstOperandCondition = isNumber && operators.length === 0;
+  const operatorsCondition = !isNumber && operators.length === 0 && isOperator;
+  const secondOperandCondition = isNumber && operators.length === 1 && equalsOperator === '';
 
-let display = firstOperandCondition ?  firstOperand += clickedValue :
-              operatorsCondition ? operators += clickedValue :
-              secondOperand += clickedValue;
+  let display = firstOperandCondition ?  firstOperand += clickedValue :
+                operatorsCondition ? operators += clickedValue :
+                secondOperand += clickedValue;
 
+  let updatedDisplay = (operators === '') ? (display += clickedValue).slice(0,-1) :
+                      (secondOperand === '') ? `${parseFloat(firstOperand)}${operators}` :
+                      `${parseFloat(firstOperand)}${operators}${parseFloat(secondOperand)}`;
 
+  console.log(updatedDisplay);
 
-if (operators === ''){
-  console.log((display += clickedValue).slice(0,-1));
+  return;
 }
-
-else if (secondOperand === '') {
-  console.log(`${parseFloat(firstOperand)}${operators}`);
-} else{
-  console.log(`${parseFloat(firstOperand)}${operators}${parseFloat(secondOperand)}` );
-}
-              
-             
-// let result = parseFloat(firstOperand) + operators + parseFloat(secondOperand);
-
-// console.log(result);
-
-
-
-
-
-
-  
-    return
-  }
-
-
-
-  // const numberArr = console.table(Array.from(buttons)).map((element) => console.table(element.innerText))
-  // console.log(numberArr);
 
     
   
